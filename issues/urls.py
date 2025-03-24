67364fb671c2f3d235fe6994b4bc71cc530dc8e9
victor.diez <04.diez.v@gmail.com>
2025-03-25 00:09:57 +0100

    Task#7 falta la barra de navegacion de los issues

--- issues/urls.py
+++ issues/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import issue_list, issue_create
+from .views import issue_list, issue_create, delete_issue
 
 urlpatterns = [
     path('', issue_list, name='issue_list'),
     path('new/', issue_create, name='issue_create'),
+    path('delete/<int:issue_id>/', delete_issue, name='delete_issue')
 ]
