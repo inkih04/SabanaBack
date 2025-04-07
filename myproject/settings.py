5fd628efdc56f441d3fdcfd2f195efbfd566bbdd
victor.diez <04.diez.v@gmail.com>
2025-04-07 12:34:37 +0200

    Task#60 Deployment configurations done

--- myproject/settings.py
+++ myproject/settings.py
@@ -53,15 +53,20 @@ INSTALLED_APPS = [
     'allauth.socialaccount.providers.google',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 MIDDLEWARE = [
     "allauth.account.middleware.AccountMiddleware",
 
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
