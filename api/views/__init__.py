9c166d35d0ee15c8efc9c82407774a48a91a3957
david.mas.escude <david.mas.escude@estudiantat.upc.edu>
2025-04-26 14:48:14 +0200

    Task #90 Add Profile and Severity viewsets with API endpoints

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,7 +1,11 @@
 from .issue_views import IssueViewSet
 from .status_views import StatusViewSet
+from .profile_views import ProfileViewSet
+from .severities_view import SeverityViewSet
 
 __all__ = [
     'IssueViewSet',
     'StatusViewSet',
+    'ProfileViewSet',
+    'SeverityViewSet',
 ]
